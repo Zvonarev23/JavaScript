4095caf05c9bb9288aa45c0f06a46959f955a92d
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2022-10-24 21:20:54 +0300

    Lesson-1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let title = "JavaScript!";
+let screens = 1920;
+let screenPrice = 20000;
+let rollback = "Html";
+let fullPrice = 25000;
+let adaptive = "Fuck off";
+
+console.log(title);
+alert("Неужели мы добрались до Javascript?");
