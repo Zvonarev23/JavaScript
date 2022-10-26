c68506ac4061dd9f88502487f9431e74a2ca53a3
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2022-10-26 20:59:07 +0300

    Finished lesson-2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Урок 1
+
 let title = "JavaScript!";
 let screens = 1920;
 let screenPrice = 20000;
@@ -6,4 +8,27 @@ let fullPrice = 25000;
 let adaptive = "Fuck off";
 
 console.log(title);
-alert("Неужели мы добрались до Javascript?");
+
+// Урок 2
+
+// Задание 1
+
+title = "JavaScript - это просто!";
+screens = "Простые, Сложные, Интерактивные"
+screenPrice = 10000;
+rollback = 20;
+fullPrice = 90000;
+adaptive = true;
+
+// Задание 2
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей")
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей")
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+console.log(fullPrice * (rollback / 100));
