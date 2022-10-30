8cdbeb3fe3a9ab762a50c7ef21c98bbd262e71df
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2022-10-30 20:32:56 +0300

    Lesson-3

--- js/script.js
+++ js/script.js
@@ -21,3 +21,45 @@ console.log("Стоимость разработки сайта" + " " + fullPri
 console.log(screens.toLowerCase());
 console.log(screens.split(", "));
 console.log(fullPrice * (rollback / 100));
+
+// Задание 3 
+
+// Спрашиваем у пользователя "Как называется ваш проект?" и результат сохраняем в переменную title 
+
+title = prompt("Как называется ваш проект?");
+
+// Спросить у пользователя "Какие типы экранов нужно разработать?" Сохранить в переменную screens
+screens = prompt("Какие типы экранов нужно разработать?");
+
+// Спросить у пользователя "Сколько будет стоить данная работа?" и сохранить данные в переменной adaptive (булево значение true/false)
+adaptive = confirm("Нужен ли адаптив на сайте?");
+
+// Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 1. "Какой дополнительный тип услуг нужен?" (Например servise1 , servise2). 2 "Сколько будет это будет стоить?" (например servicePrice1, servisePrice2)
+
+let servise1 = prompt("Какой дополнительный тип услуг нужен?");
+let servisePrice1 = +prompt("Сколько это будет стоить?");
+
+let servise2 = prompt("Какой дополнительный тип услуг нужен?");
+let servisePrice2 = +prompt("Сколько это будет стоить?");
+
+// Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servisePrice + servisePrice2) и результат занести в переменную fullPrice 
+fullPrice = screenPrice + servisePrice1 + servisePrice2;
+
+// Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику (servicePercentPrice = fullPrice - Откат посреднику), округлив результат в большую сторону (методы объекта Math в помощь). Вывести servicePercentPrice в консоль
+
+let servicePercentPrice = Math.ceil(fullPrice - 1500);
+console.log(servicePercentPrice);
+
+// Написать конструкцию условий (расчеты приведены в рублях) 
+
+if (fullPrice > 30000) {
+    console.log("Даем скидку в 10%");
+} else if (15000 <= fullPrice && fullPrice <= 30000) {
+    console.log("Даем скидку в 5%");
+} else if (0 <= fullPrice && fullPrice < 15000) {
+    console.log("Скидка не предусмотрена");
+} else 
+  console.log("Что-то пошло не так");
+
+
+    
