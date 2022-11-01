2bccd7005f2259bf62aa9e37c66ee106c600e4c4
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2022-11-01 18:57:02 +0300

    lesson03: fixed bugs

--- js/script.js
+++ js/script.js
@@ -2,12 +2,8 @@
 
 // Задание 1
 
-let title = "JavaScript - это просто!";
-let screens = "Простые, Сложные, Интерактивные"
 let screenPrice = 10000;
 let rollback = 20;
-let fullPrice = 90000;
-let adaptive = true;
 
 // Задание 2
 
@@ -26,13 +22,13 @@ console.log(fullPrice * (rollback / 100));
 
 // Спрашиваем у пользователя "Как называется ваш проект?" и результат сохраняем в переменную title 
 
-title = prompt("Как называется ваш проект?");
+let title = prompt("Как называется ваш проект?");
 
 // Спросить у пользователя "Какие типы экранов нужно разработать?" Сохранить в переменную screens
-screens = prompt("Какие типы экранов нужно разработать?");
+let screens = prompt("Какие типы экранов нужно разработать?");
 
 // Спросить у пользователя "Сколько будет стоить данная работа?" и сохранить данные в переменной adaptive (булево значение true/false)
-adaptive = confirm("Нужен ли адаптив на сайте?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
 // Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 1. "Какой дополнительный тип услуг нужен?" (Например servise1 , servise2). 2 "Сколько будет это будет стоить?" (например servicePrice1, servisePrice2)
 
@@ -43,7 +39,7 @@ let servise2 = prompt("Какой дополнительный тип услуг
 let servisePrice2 = +prompt("Сколько это будет стоить?");
 
 // Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servisePrice + servisePrice2) и результат занести в переменную fullPrice 
-fullPrice = screenPrice + servisePrice1 + servisePrice2;
+let fullPrice = screenPrice + servisePrice1 + servisePrice2;
 
 // Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику (servicePercentPrice = fullPrice - Откат посреднику), округлив результат в большую сторону (методы объекта Math в помощь). Вывести servicePercentPrice в консоль
 
@@ -58,8 +54,8 @@ if (fullPrice > 30000) {
     console.log("Даем скидку в 5%");
 } else if (0 <= fullPrice && fullPrice < 15000) {
     console.log("Скидка не предусмотрена");
-} else 
+} else {
   console.log("Что-то пошло не так");
-
+}
 
     
